9cf50fb8029fe24871e013444c43b8d4934f1d47
mandzo-velibor <velja.jagodina@gmail.com>
2026-05-01 22:12:13 +0200

    Fix Vercel root detection and studio gitlink

---
+++ studio/schemaTypes/productType.ts
@@ -0,0 +1,22 @@
+export const productType = {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Naziv',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Opis',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      title: 'Slika',
+      type: 'image',
+    },
+  ],
+}
